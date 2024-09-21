1548256d446ebaf167a6498be96b08ce34b0ccc8
Jesus Amador <jesusac1992@gmail.com>
2024-09-21 15:55:39 -0600

    Update and rename manifest.js to service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'arpal-wrapper-v1';
+const urlsToCache = [
+  '/Arpal/',
+  '/Arpal/index.html',
+  '/Arpal/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
